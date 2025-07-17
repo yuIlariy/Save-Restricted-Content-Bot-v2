f018c041b3b50ff9cb1aaaf7401564bf93962942
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-17 15:10:28 +0300

    Update ytdl.py

--- devgagan/modules/ytdl.py
+++ devgagan/modules/ytdl.py
@@ -122,7 +122,7 @@ async def process_audio(client, event, url, cookies_env_var=None):
                     pass
                 audio_file.tags["TIT2"] = TIT2(encoding=3, text=title)
                 audio_file.tags["TPE1"] = TPE1(encoding=3, text="Team SPY")
-                audio_file.tags["COMM"] = COMM(encoding=3, lang="eng", desc="Comment", text="Processed by Team SPY")
+                audio_file.tags["COMM"] = COMM(encoding=3, lang="eng", desc="Comment", text="Processed by NAm")
  
                 thumbnail_url = info_dict.get('thumbnail')
                 if thumbnail_url:
@@ -150,7 +150,7 @@ async def process_audio(client, event, url, cookies_env_var=None):
                 name=None,
                 progress_bar_function=lambda done, total: progress_callback(done, total, chat_id)
             )
-            await client.send_file(chat_id, uploaded, caption=f"**{title}**\n\n**__Powered by Team SPY__**")
+            await client.send_file(chat_id, uploaded, caption=f"**{title}**\n\n**__Powered by NAm__**")
             if prog:
                 await prog.delete()
         else:
@@ -304,7 +304,7 @@ def progress_callback(done, total, user_id):
         f"│ **__Speed:__** {speed_mbps:.2f} Mbps\n"
         f"│ **__Time Remaining:__** {remaining_time_min:.2f} min\n"
         f"╰──────────────────╯\n\n"
-        f"**__Powered by Team SPY__**"
+        f"**__Powered by NAm__**"
     )
  
      
