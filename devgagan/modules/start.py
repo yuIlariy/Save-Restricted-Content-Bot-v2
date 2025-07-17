eacb2661bbfb02427c3e9753b7e69a5cde8fbedb
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-17 16:08:46 +0300

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -109,7 +109,7 @@ help_pages = [
         "> 4. REPLACEWORDS : Can be used for words in deleted set via REMOVE WORDS\n"
         "> 5. RESET : To set the things back to default\n\n"
         "> You can set CUSTOM THUMBNAIL, PDF WATERMARK, VIDEO WATERMARK, SESSION-based login, etc. from settings\n\n"
-        "**__Powered by Team SPY__**"
+        "**__Powered by NAm__**"
     )
 ]
  
