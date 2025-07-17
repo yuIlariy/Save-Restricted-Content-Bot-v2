14d6955ada7dd94bf7129483c0fa452f672797f5
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-17 18:37:26 +0300

    Update ytdl.py

--- devgagan/modules/ytdl.py
+++ devgagan/modules/ytdl.py
@@ -455,9 +455,12 @@ async def split_and_upload_file(app, sender, file_path, caption):
 
             # Uploading part
             edit = await app.send_message(sender, f"⬆️ Uploading part {part_number + 1}...")
-            part_caption = f"{caption} \n\n**Part : {part_number + 1}**"
-            await app.send_document(sender, document=part_file, caption=part_caption,
-                progress=progress_bar,
+            part_caption = f"{caption}"  # Simple caption without part number
+            await app.send_document(
+        sender,
+        document=file_path,
+        caption=caption,
+        progress=progress_callback,
                 progress_args=("╭─────────────────────╮\n│      **__Pyro Uploader__**\n├─────────────────────", edit, time.time())
             )
             await edit.delete()
